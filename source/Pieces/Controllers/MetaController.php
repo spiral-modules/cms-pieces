7b3e1cc04c1101b645b3d2cb8402b495f564d4d8
Dmitry Mironov <dmitry.mironov@spiralscout.com>
2017-03-10 02:04:56 +0300

    Add get page metadata action

--- source/Pieces/Controllers/MetaController.php
+++ source/Pieces/Controllers/MetaController.php
@@ -57,4 +57,41 @@ class MetaController extends Controller
             'metaID' => $meta->primaryKey()
         ];
     }
+
+    /**
+     * @param MetaRequest  $request
+     * @param Pieces       $pieces
+     * @param PiecesConfig $config
+     * @return array
+     */
+    public function getAction(MetaRequest $request, Pieces $pieces, PiecesConfig $config): array
+    {
+        $this->authorize($config->cmsPermission());
+
+        if (!$request->isValid()) {
+            return [
+                'status' => 400,
+                'errors' => $request->getErrors()
+            ];
+        }
+
+        if (empty($meta = $pieces->findMeta($request->namespace, $request->view, $request->code))) {
+            return [
+                'status' => 400,
+                'error'  => $this->say('Unable to find requested meta')
+            ];
+        }
+
+        return [
+            'status' => 200,
+            'piece'  => [
+                'data' => [
+                    'html'        => $meta->html,
+                    'title'       => $meta->title,
+                    'description' => $meta->description,
+                    'keywords'    => $meta->keywords,
+                ],
+            ]
+        ];
+    }
 }
